31f90643e97eb00b866e31f7a7a280af56b28bc9
Yeralyn Rodríguez <yerba1108@gmail.com>
2021-08-26 14:03:39 -0600

    Cambio ye

---
+++ ProyectoSuppcenter/Models/Usuario/Prueba_Yeralyn.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProyectoSuppcenter.Models.Usuario
+{
+    public class Prueba_Yeralyn
+    {
+        int Numero;
+        int nombre;
+        //ramas
+    }
+}
